0cba9d864b2a5e1087a4f078de9184abf0dfc763
Vladimir Orekhov <vladimir.orekhov@booking.com>
2018-07-11 19:15:25 +0200

    Implement simple random quote generator

--- docker/app.py
+++ docker/app.py
@@ -1,11 +1,27 @@
 from flask import Flask
+import random
 
 # create the Flask application
 app = Flask(__name__)
+quotes = []
+
+
+def loadQuotes():
+    file = open("quotes.txt", "r")
+    for line in file:
+        line = line.rstrip()
+        if not line:
+            break
+        quotes.append(line.split("\t"))
+
 
 @app.route("/", methods=["GET"])
-def list_cars():
-    return "Hello, world of 2018!"
+def get_random_quote():
+    n = len(quotes)
+    r = random.randint(0, n - 1)
+    return quotes[r][1]
+
 
 if __name__ == "__main__":
+    loadQuotes()
     app.run(host='0.0.0.0')
